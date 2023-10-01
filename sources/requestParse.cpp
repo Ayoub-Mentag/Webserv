c90b2f3008420c7e59f8f59baad2b67b4611200a
mtigunit <matadormat333@gmail.com>
2023-10-01 12:45:20 +0100

    parsing

--- sources/requestParse.cpp
+++ sources/requestParse.cpp
@@ -4,12 +4,14 @@ void 	requestParse(t_request& request, std::string buffer) {
 	std::istringstream	iss(buffer);
 	std::string			str;
 
+	// request line 
 	std::getline(iss, str);
 	size_t first = str.find_first_of(' ');
 	size_t last = str.find_last_of(' ');
 	request.method = str.substr(0, first);
 	request.httpVersion = str.substr(last + 1, -1);
 	request.path = str.substr(first + 1, last - first - 1);
+
 	std::getline(iss, str);
 	last = str.find_last_of(':');
 	request.port = std::atoi(str.substr(last + 1, -1).c_str());
